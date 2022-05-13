945d27b31c2ae9b8f702bad4950c0b0fff38e3ed
aimeiyijia <1456723200@qq.com>
2022-05-13 16:32:43 +0800

    feat：调整指令写法

--- src/components/directives/height-adaptive.ts
+++ src/components/directives/height-adaptive.ts
@@ -40,29 +40,34 @@ const calcTableHeight = (element: IHTMLElement, offset: IOffset) => {
   return height
 }
 
-const doResize = (el: IHTMLElement, binding: DirectiveBinding) => {
+const doResize = (el: IHTMLElement, binding: DirectiveBinding, vnode: VNode) => {
   const height = calcTableHeight(el, binding.value)
   el.style.height = `${height}px`
+  const {componentInstance: $ElLists} = vnode!.children![0] as any
+  if($ElLists){
+    console.log($ElLists, '实例')
+    $ElLists.update()
+  }
 }
 
 const directive: DirectiveOptions = {
   bind(el, binding, vnode) {
     const elType = el as unknown as IHTMLElement
-    const resizeListener = () => doResize(elType, binding)
+    const resizeListener = () => doResize(elType, binding, vnode)
     globalEventListener.f = debounce(resizeListener, 100)
     window.addEventListener('resize', globalEventListener.f)
     // 立刻执行一次
-    doResize(elType, binding)
+    doResize(elType, binding, vnode)
   },
   update(el, binding, vnode) {
     window.removeEventListener('resize', globalEventListener.f)
 
     const elType = el as unknown as IHTMLElement
-    const resizeListener = () => doResize(elType, binding)
+    const resizeListener = () => doResize(elType, binding, vnode)
     globalEventListener.f = debounce(resizeListener, 100)
     window.addEventListener('resize', globalEventListener.f)
 
-    doResize(el as unknown as IHTMLElement, binding)
+    doResize(el as unknown as IHTMLElement, binding, vnode)
   },
   unbind() {
     window.removeEventListener('resize', globalEventListener.f)
