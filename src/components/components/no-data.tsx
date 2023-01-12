404fe7ec5189ccc1d5e506897c8c6a7dbf3f1e6a
aimeiyijia <1456723200@qq.com>
2023-01-12 16:41:23 +0800

    feat：调整无数据时提示

--- src/components/components/no-data.tsx
+++ src/components/components/no-data.tsx
@@ -1,6 +1,5 @@
 import { Vue, Component } from 'vue-property-decorator'
 import { VNode, CreateElement } from 'vue'
-const noDataPng = require('../assets/images/noData.png')
 @Component({
   name: 'NOData',
 })
@@ -8,8 +7,7 @@ export default class extends Vue {
   render(h: CreateElement): VNode {
     return (
       <div class="el-lists_nodata">
-        <el-image src={ noDataPng }></el-image>
-        <span>暂无数据</span>
+        <el-empty></el-empty>
       </div>
     )
   }
